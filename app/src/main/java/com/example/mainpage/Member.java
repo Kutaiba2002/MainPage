a4f156c624e7b3b22c87be1df2a46036bc508f9a
Kutaiba2002 <kingkutaiba36@gmail.com>
2023-06-27 16:40:55 +0300

    V1

---
+++ app/src/main/java/com/example/mainpage/Member.java
@@ -0,0 +1,38 @@
+package com.example.mainpage;
+
+public class Member {
+
+    private String name;
+    private String Email;
+    private String pass;
+
+    public Member(String name, String email, String pass) {
+        this.name = name;
+        Email = email;
+        this.pass = pass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
