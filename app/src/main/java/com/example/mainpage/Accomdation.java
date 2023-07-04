c94cb130942463fe26a5b89210074f4b71ee9d08
Kutaiba2002 <kingkutaiba36@gmail.com>
2023-07-04 19:11:39 +0300

    V3

--- app/src/main/java/com/example/mainpage/Accomdation.java
+++ app/src/main/java/com/example/mainpage/Accomdation.java
@@ -21,14 +21,14 @@ public class Accomdation extends AppCompatActivity implements RecyclerInterface 
         setContentView(R.layout.activity_accomdation);
 
         roomArrayList = new ArrayList<>();
-        roomArrayList.add(new RoomDetails(R.drawable.single_room,50,"Single Room",1,""));
-        roomArrayList.add(new RoomDetails(R.drawable.double_bedroom,100,"Double Room",2,""));
-        roomArrayList.add(new RoomDetails(R.drawable.suite,80,"Suite Room",3,""));
-        roomArrayList.add(new RoomDetails(R.drawable.studio,70,"Studio Room",2,""));
-        roomArrayList.add(new RoomDetails(R.drawable.connecting,150,"Connecting Room",4,""));
-        roomArrayList.add(new RoomDetails(R.drawable.granda,120,"Granda Room",2,""));
-        roomArrayList.add(new RoomDetails(R.drawable.adjacent,200,"Adjacent Room",5,""));
-        roomArrayList.add(new RoomDetails(R.drawable.deluxe,150,"Deluxe Room",1,""));
+        roomArrayList.add(new RoomDetails(R.drawable.single_room,50,"Single Room",1,"A single room is designed to accommodate a single guest. It typically consists of a single bed and basic amenities."));
+        roomArrayList.add(new RoomDetails(R.drawable.double_bedroom,100,"Double Room",2,"A double room is suitable for two guests and usually contains a double or queen-sized bed. It provides space and amenities for two people to stay comfortably."));
+        roomArrayList.add(new RoomDetails(R.drawable.suite,80,"Suite Room",3,"A suite is a larger and more luxurious room option. It often includes a separate living area and bedroom. Suites may also feature additional amenities such as a kitchenette, dining area, or even a private balcony."));
+        roomArrayList.add(new RoomDetails(R.drawable.studio,70,"Studio Room",2,"A studio room is an open-plan space that combines a living area, bedroom, and kitchenette in a single room. It is designed to offer a compact and self-contained accommodation option."));
+        roomArrayList.add(new RoomDetails(R.drawable.connecting,150,"Connecting Room",4,"Connecting rooms are two separate rooms with a connecting door or corridor. They are convenient for families or groups who want to stay close to each other while having their own private space."));
+        roomArrayList.add(new RoomDetails(R.drawable.granda,120,"Granda Room",2,"A grand suite is an upgraded and more spacious version of a regular suite. It offers enhanced amenities, additional living space, and often includes luxurious features like a private jacuzzi or panoramic views."));
+        roomArrayList.add(new RoomDetails(R.drawable.adjacent,200,"Adjacent Room",5,"Adjacent rooms are rooms located next to each other but without a connecting door. They are suitable for guests who want to stay close to each other while maintaining separate accommodations."));
+        roomArrayList.add(new RoomDetails(R.drawable.deluxe,150,"Deluxe Room",1,"A deluxe room is a higher-tier room option that offers superior comfort and amenities compared to standard rooms. It may feature upgraded furnishings, better views, and additional amenities like a minibar or a larger bathroom."));
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2);
@@ -43,7 +43,7 @@ public class Accomdation extends AppCompatActivity implements RecyclerInterface 
         intent.putExtra("name", roomDetails.getName());
         intent.putExtra("image", roomDetails.getImage());
         intent.putExtra("price", roomDetails.getPrice());
-        intent.putExtra("person", roomDetails.getNumberOfPerson());
+        intent.putExtra("description", roomDetails.getDescription());
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/mainpage/ShowDetails.java
+++ app/src/main/java/com/example/mainpage/ShowDetails.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ShowDetails extends AppCompatActivity {
 
@@ -14,6 +18,9 @@ public class ShowDetails extends AppCompatActivity {
     private ImageView image;
     private TextView text2;
     private TextView text3;
+    private Button button;
+    private FirebaseFirestore database = FirebaseFirestore.getInstance();
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,17 +31,28 @@ public class ShowDetails extends AppCompatActivity {
         String name = intent.getExtras().getString("name");
         int id = intent.getIntExtra("image", 0);
         int price = intent.getIntExtra("price", 0);
-        int person = intent.getIntExtra("person", 0);
+        String description = intent.getStringExtra("description");
+ //       int person = intent.getIntExtra("person", 0);
 
         text1 = findViewById(R.id.nameTxt);
         image = findViewById(R.id.image);
         text2 = findViewById(R.id.textPrice);
         text3 = findViewById(R.id.textDescription);
+        button = findViewById(R.id.button);
 
         text1.setText(name);
         image.setImageResource(id);
         text2.setText(String.valueOf(price)+"$/day");
-        text3.setText(String.valueOf(person)+" Person");
+        text3.setText(description);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+//        text3.setText(String.valueOf(person)+" Person");
 
     }
 }
